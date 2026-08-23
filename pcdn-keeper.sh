b2471c669f14b429dc0886732fa730b576580de2
pandamelive <59890056+pandamelive@users.noreply.github.com>
2026-08-24 07:12:58 +0800

    Update pcdn-keeper.sh

--- pcdn-keeper.sh
+++ pcdn-keeper.sh
@@ -16,7 +16,7 @@ while true; do
       --max-overall-download-limit=100M \
       --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" \
       --connect-timeout=15 \
-      --timeout=3600 \
+      --timeout=600 \
       --retry-wait=10 \
       --max-tries=5 \
       "http://updates-http.cdn-apple.com/2019WinterFCS/fullrestores/041-39257/32129B6C-292C-11E9-9E72-4511412B0A59/iPhone_4.7_12.1.4_16D57_Restore.ipsw" || \
